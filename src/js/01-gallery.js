782545dcd9ba9dc1761e38e6dad5e33c036649f8
R0m5 <rlastovetskyi@gmail.com>
2023-03-05 21:10:07 +0300

    +

---
+++ src/js/01-gallery.js
@@ -0,0 +1,26 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+const imageContainer = document.querySelector('.gallery');
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+imageContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createImagesMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `      
+     <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join('');
+}
+
+new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+  navText: ['❰', '❱'],
+});
+
+console.log(galleryItems);
