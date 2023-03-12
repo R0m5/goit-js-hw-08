9f47a6ac50c20014ad1401ee209069297e7fb228
R0m5 <rlastovetskyi@gmail.com>
2023-03-12 19:04:56 +0300

    Did the third task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,20 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const SRORAGE_KEY = 'videoplayer-current-time';
+const THROTTLE_DELAY = 1000;
+
+const onPlay = function (data) {
+  localStorage.setItem(SRORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', onPlay, THROTTLE_DELAY);
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
+}
+
 console.log(Player);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const email = document.querySelector('input');
+const message = document.querySelector('textarea');
+const STORAGE_KEY = 'feedback-form-state';
+const formData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
+
+function onFormInput() {
+  localStorage.setItem(
+    STORAGE_KEY,
+    JSON.stringify({ email: email.value, message: message.value })
+  );
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  evt.currentTarget.reset();
+  console.log(localStorage.getItem(STORAGE_KEY));
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function getValueFromLocalStorage() {
+  if (formData === null) {
+    return;
+  }
+  email.value = formData.email;
+  message.value = formData.message;
+}
+
+getValueFromLocalStorage();
